148aad203c7f4cf9674900532e1b8c5650e9a8d0
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2023-05-02 21:24:11 +0300

    Added Setters and Getters

--- Order.java
+++ Order.java
@@ -11,4 +11,37 @@ public class Order {
     public String GetDDate(){
         return DeliveryDate;
     }
+    public int getId() {
+        return Id;
+    }
+    public double getTotalPrice() {
+        return TotalPrice;
+    }
+    public String getODate() {
+        return OrderedDate;
+    }
+    public String getPaymentMethod() {
+        return PaymentMethod;
+    }
+    public String getStatus() {
+        return Status;
+    }
+    public void setDDate(String deliveryDate) {
+        DeliveryDate = deliveryDate;
+    }
+    public void setOProducts(Vector<Product> OProducts) {
+        this.OProducts = OProducts;
+    }
+    public void setDate(String orderedDate) {
+        OrderedDate = orderedDate;
+    }
+    public void setPaymentMethod(String paymentMethod) {
+        PaymentMethod = paymentMethod;
+    }
+    public void setStatus(String status) {
+        Status = status;
+    }
+    public void setTotalPrice(double totalPrice) {
+        TotalPrice = totalPrice;
+    }
 }
