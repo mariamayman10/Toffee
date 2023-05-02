f3556a22a615c9a6ece3ad15637aa5904f6ecc37
Habiba Ihab <101571848+habibaihab@users.noreply.github.com>
2023-05-03 02:16:55 +0300

    addItemToCart

--- Cart.java
+++ Cart.java
@@ -1,2 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
 public class Cart {
+    private Map<Product,Integer>cart;
+    public Cart(){
+        cart= new HashMap<Product, Integer>();
+    }
+    public void addItem(Product product , int quantity) {
+        Integer Quantites = cart.get(product);
+        if(Quantites != null){
+            quantity += Quantites;
+        }else{
+            cart.put(product,quantity);
+        }
+    }
 }
