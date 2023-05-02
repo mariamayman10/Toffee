e90534e2f391ee34e4f459f5158f1c1537aca653
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2023-05-02 21:53:28 +0300

    Added Attributes, Setters, and Getters

--- Product.java
+++ Product.java
@@ -1,2 +1,32 @@
 public class Product {
+    private double Price;
+    private int Discount;
+    private int Id;
+    String Name;
+    String Category;
+
+    public int getDiscount() {
+        return Discount;
+    }
+    public int getId() {
+        return Id;
+    }
+    public double getPrice() {
+        return Price;
+    }
+    public String getCategory() {
+        return Category;
+    }
+    public String getName() {
+        return Name;
+    }
+    public void setDiscount(int discount) {
+        Discount = discount;
+    }
+    public void setPrice(int price) {
+        Price = price;
+    }
+    Product(int id, int discount, double price, String name, String category){
+        Id = id;Discount = discount;Price = price;Name = name;Category = category;
+    }
 }
