f1f4d2b378e9edf7ab73c8d072f4c1ed2845d9ac
Habiba Ihab <101571848+habibaihab@users.noreply.github.com>
2023-05-02 23:58:12 +0300

    add function ShowProducts()

--- Catalog.java
+++ Catalog.java
@@ -1,2 +1,15 @@
+import java.util.Vector;
+
 public class Catalog {
+    Vector<Product>products;
+    public Catalog(){
+        this.products=new Vector<Product>();
+    }
+    public void showProducts(){
+        for(Product p : products){
+            System.out.println(p.getName() + "-"+ p.getPrice() + "-" + p.getCategory());
+        }
+
+    }
+
 }
