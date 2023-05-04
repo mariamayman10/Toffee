95ad03abc92debc8ca69ffea3d49c26866f1df5b
Aya Ali <101468593+AyaA1i@users.noreply.github.com>
2023-05-04 17:36:35 +0300

    Update Catalog.java

--- Catalog.java
+++ Catalog.java
@@ -1,15 +1,22 @@
 import java.util.Vector;
 
 public class Catalog {
-    Vector<Product>products;
-    public Catalog(){
-        this.products=new Vector<Product>();
-    }
-    public void showProducts(){
-        for(Product p : products){
-            System.out.println(p.getName() + "-"+ p.getPrice() + "-" + p.getCategory());
+    public static Vector<Product> catalog = new Vector<Product>();
+
+    public void ShowCatalog(){
+        for (int i = 0; i < catalog.size(); i++){
+            Product P = catalog.get(i);
+            System.out.print("Product Name: ");
+            P.getName();
+            System.out.print("\nProduct Category: ");
+            P.getCategory();
+            if(P.getDiscount() != 0){
+                double NewPrice = P.getPrice() - ((P.getDiscount() / 100) * P.getPrice());
+                System.out.print("\nThere is a Discount! Product Price After Discount: ");
+                P.getPrice();
+            }
+            System.out.print("\nProduct Price: ");
+            P.getPrice();
         }
-
     }
-
 }
