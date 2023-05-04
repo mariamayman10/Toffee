f45bbd363bcad3c5b454b4fc2d8f726da0611481
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2023-05-04 20:24:07 +0300

    Update MySystem.java

--- MySystem.java
+++ MySystem.java
@@ -1,5 +1,3 @@
-import javax.naming.Name;
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class MySystem {
@@ -7,25 +5,39 @@ public class MySystem {
     Database DB = new Database();
     public void EnterApplication(){
         System.out.println("_________Welcome to Toffee_________");
-
     }
-    /////////////////////////////////////////(last)
-    public void LoggedIn(Customer cust){
-
-    }
-    public void ShowMenu(){
-        while(true){
-            System.out.println("Go to: 1)SignUp 2)Login 3)View Products 4)Exit");
+    public void LoggedIn(Customer customer){
+        while(true) {
+            System.out.println("Want: 1)View Products 2)Add Item To Cart 3)Place Order 4)Log Out");
             int choice = in.nextInt();
-            while (choice != 1 && choice != 2 && choice != 3 && choice != 4 ){
+            while (choice != 1 && choice != 2 && choice != 3 && choice != 4) {
                 System.out.println("Please Choose Either 1, 2 , 3 or 4");
                 choice = in.nextInt();
             }
-            if(choice == 1)ShowSignUp();
-            else if(choice == 2)ShowLogIn();
-            else if(choice == 3) ShowCatalog();
-            else break;
+            if (choice == 1) ShowCatalog();
+            else if (choice == 2) {
+                System.out.println("Please Enter the id of the product: ");
+                int Pid = in.nextInt();
+                System.out.println("Please Enter the quantity of the product: ");
+                Integer Quantity = in.nextInt();
+                Product Product = new Product(DB.SearchforProduct(Pid));
+                customer.custCart.addItem(Product, Quantity);
+                customer.custCart.viewCart();
+            } else if (choice == 3) customer.Checkout();
+            else System.exit(0);
         }
+    }
+    public void ShowMenu(){
+        System.out.println("Go to: 1)SignUp 2)Login 3)View Products 4)Exit");
+        int choice = in.nextInt();
+        while (choice != 1 && choice != 2 && choice != 3 && choice != 4){
+            System.out.println("Please Choose Either 1, 2 , 3 or 4");
+            choice = in.nextInt();
+        }
+        if(choice == 1)ShowSignUp();
+        else if(choice == 2)ShowLogIn();
+        else if(choice == 3) ShowCatalog();
+        else System.exit(0);
     }
     public void ShowSignUp(){
         System.out.println("____SIGN UP____");
@@ -49,6 +61,7 @@ public class MySystem {
             System.out.print("\nEnter Your Address: ");
             Add = in.next();
         }
+        ShowMenu();
     }
     public void ShowLogIn(){
         System.out.println("____LOG IN____");
@@ -56,45 +69,48 @@ public class MySystem {
         String Email = in.next();
         System.out.print("\nEnter Your Password: ");
         String Pass = in.next();
-        Customer cust = DB.ValidateAcc(Email, Pass);
-        while(cust!=null){
+        Customer customer = new Customer();
+        customer = DB.ValidateAcc(Email, Pass);
+        while(customer == null){
             Failure();
             System.out.println("ForgotPassword?");
             String Reply = in.next();
-            if(Reply.toLowerCase().equals("yes")){
+            if(Reply.equalsIgnoreCase("yes")){
                 System.out.print("\nEnter Your Email: ");
                 Email = in.next();
                 System.out.print("\nEnter Your New Password: ");
                 String NPass = in.next();
                 DB.SavePassword(Email, NPass);
+                customer = DB.ValidateAcc(Email, Pass);
             }
-            System.out.println("____LOG IN____");
-            System.out.print("\nEnter Your Email: ");
-            Email = in.next();
-            System.out.print("\nEnter Your Password: ");
-            Pass = in.next();
+            else{
+                System.out.println("____LOG IN____");
+                System.out.print("\nEnter Your Email: ");
+                Email = in.next();
+                System.out.print("\nEnter Your Password: ");
+                Pass = in.next();
+                customer = DB.ValidateAcc(Email, Pass);
+            }
         }
-
-        LoggedIn(cust);
-
+        LoggedIn(customer);
     }
     public void ShowCatalog(){
-       for(int i =0;i< DB.getCatalog().size();i++){
-           Product P =new Product(DB.getCatalog().get(i));
-           System.out.print("Product Name: ");
-           System.out.print(P.getName());
-           System.out.print("\nProduct Category: ");
-           System.out.print(P.getCategory());
-           if(P.getDiscount() != 0){
-               double NewPrice = P.getPrice() - ((P.getDiscount() / 100) * P.getPrice());
-               System.out.print("\nThere is a Discount! Product Price After Discount: ");
-               System.out.print(NewPrice);
-           }
-           System.out.print("\nProduct Price: ");
-           System.out.println(P.getPrice());
-           System.out.println("----------------------------------------------------");
+        for(int i =0;i< DB.getCatalog().size();i++){
+            Product P =new Product(DB.getCatalog().get(i));
+            System.out.print("Product Name: ");
+            System.out.print(P.getName());
+            System.out.print("\nProduct Category: ");
+            System.out.print(P.getCategory());
+            if(P.getDiscount() != 0){
+                double NewPrice = P.getPrice() - ((P.getDiscount() / 100) * P.getPrice());
+                System.out.print("\nThere is a Discount! Product Price After Discount: ");
+                System.out.print(NewPrice);
+            }
+            System.out.print("\nProduct Price: ");
+            System.out.println(P.getPrice());
+            System.out.println("----------------------------------------------------");
 
-       }
+        }
     }
     public void Failure(){
         System.out.println("Action Couldn't Be Applied..Try Again");
