217337218f1576049575feeda64edd9cded968e1
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2023-05-06 01:11:09 +0300

    Update Cart.java

--- Cart.java
+++ Cart.java
@@ -2,8 +2,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Cart {
-    private static Map<Product,Integer> cart = new HashMap<>();
+    private static final Map<Product,Integer> cart = new HashMap<>();
     public void addItem(Product product, Integer quantity) {
+        for (Map.Entry<Product, Integer> pair : cart.entrySet()) {
+            if(product.getID() == pair.getKey().getID()){
+                pair.setValue(pair.getValue() + quantity);
+                return;
+            }
+        }
         cart.put(product,quantity);
     }
     public void clearCart() {
@@ -11,7 +17,11 @@ public class Cart {
     }
     public void viewCart(){
         for (Map.Entry<Product, Integer> entry : cart.entrySet()) {
-            System.out.println(entry.getKey().Name + " = " + entry.getValue());
+            System.out.println(entry.getKey().getName() + " = " + entry.getValue());
         }
+    }
+
+    public Map<Product, Integer> getCart() {
+        return cart;
     }
 }
