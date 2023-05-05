4bd87acfc59d59aef1292ad584edae8037bc277b
Aya Ali <101468593+AyaA1i@users.noreply.github.com>
2023-05-05 18:04:12 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-       Catalog atalog = new Catalog();
-       for(int i = 0;i<5;i++){
-           Product pro = new Product(10.0+i,"Aya"+i,"C"+i,1.0+i,0+i,0+i);
-           atalog.catalog.add(pro);
-       }
+       Catalog myCatalog = new Catalog();
+       myCatalog.fillCatalog();
        MySystem ss = new MySystem();
        ss.EnterApplication();
        ss.ShowMenu();
-
-       //ss.ShowCatalog();
     }
 }
