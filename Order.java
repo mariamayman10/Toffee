3ec267efda10bf54cebd9a32299b9f17db5fa8bd
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2023-05-06 01:12:49 +0300

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,39 +1,32 @@
-import java.util.Vector;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 public class Order {
     private int Id;
     private String Status;
     private String DeliveryDate;
-    private String OrderedDate;
     private String PaymentMethod;
     private double TotalPrice;
-    private Vector<Product> OProducts;
+    private static final Map<Product,Integer> OProducts = new HashMap<>();
     public String GetDDate(){
         return DeliveryDate;
-    }
-    public int getId() {
-        return Id;
     }
     public double getTotalPrice() {
         return TotalPrice;
     }
-    public String getODate() {
-        return OrderedDate;
+    public Map<Product, Integer> getOProducts() {
+        return OProducts;
     }
-    public String getPaymentMethod() {
-        return PaymentMethod;
-    }
-    public String getStatus() {
-        return Status;
+    public void setOProducts(Map<Product, Integer> cart){
+        for (Map.Entry<Product, Integer> pair : cart.entrySet()) {
+            Product key = pair.getKey();
+            Integer value = pair.getValue();
+            OProducts.put(key, value);
+        }
     }
     public void setDDate(String deliveryDate) {
         DeliveryDate = deliveryDate;
-    }
-    public void setOProducts(Vector<Product> OProducts) {
-        this.OProducts = OProducts;
-    }
-    public void setDate(String orderedDate) {
-        OrderedDate = orderedDate;
     }
     public void setPaymentMethod(String paymentMethod) {
         PaymentMethod = paymentMethod;
@@ -42,9 +35,18 @@ public class Order {
         Status = status;
     }
     public void setTotalPrice(double totalPrice) {
+        totalPrice = 0.0;
+        for (Map.Entry<Product, Integer> pair : OProducts.entrySet()) {
+            Product key = pair.getKey();
+            Integer value = pair.getValue();
+            totalPrice += value*(key.getPrice() - (key.getDiscount()/100)*key.getPrice());
+        }
         TotalPrice = totalPrice;
     }
     public void setID(int id) {
         Id = id;
+    }
+    public int getID() {
+        return Id;
     }
 }
