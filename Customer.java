54279f7486b1983e7583e4ca6c9117c08ed148f1
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2023-05-06 01:11:46 +0300

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -3,11 +3,11 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.Vector;
 public class Customer{
-    protected String Email;
-    protected String Password;
-    protected String Address;
-    protected int Points;
-    protected Vector<Order>ordersMade = new Vector<>();
+    private String Email;
+    private String Password;
+    private String Address;
+    private int Points;
+    private Vector<Order>ordersMade = new Vector<>();
     public Cart custCart = new Cart();
     public String getEmail(){
         return Email;
@@ -34,23 +34,23 @@ public class Customer{
         custCart = other.custCart;
     }
     public Customer(){}
-    public void Checkout()
+    public Order Checkout()
     {
         Order order = new Order();
-        Database database = new Database();
         Scanner in = new Scanner(System.in);
-        order.setOProducts(Cart.getCart());
+        order.setOProducts(custCart.getCart());
+        order.setTotalPrice(0);
+        System.out.println("Your Order Contains: ");
+        custCart.viewCart();
         order.setStatus("Not Delivered");
-        System.out.println("Enter the delivery date of the order you want to pay :");
+        System.out.println("Enter the delivery date of the order:");
         order.setDDate(in.next());
-        System.out.println("Enter the order date of the order you want to pay :");
-        order.setDate(in.next());
-        System.out.println("Enter the payment method of the order you want to pay :");
+        System.out.println("Enter the payment method of the order you want to pay:");
         order.setPaymentMethod(in.next());
         System.out.println("The total price of Your Order: " + order.getTotalPrice());
-        database.SaveOrder(order);
         ordersMade.add(order);
         custCart.clearCart();
         System.out.println("Done.");
+        return order;
     }
 }
