4f04e0a2ba8cadb7179736cbdbf113f982d01ea5
Aya Ali <101468593+AyaA1i@users.noreply.github.com>
2023-05-05 18:02:48 +0300

    Update Catalog.java

--- Catalog.java
+++ Catalog.java
@@ -1,22 +1,17 @@
 import java.util.Vector;
 
 public class Catalog {
-    public static Vector<Product> catalog = new Vector<Product>();
-
-    public void ShowCatalog(){
-        for (int i = 0; i < catalog.size(); i++){
-            Product P = catalog.get(i);
-            System.out.print("Product Name: ");
-            P.getName();
-            System.out.print("\nProduct Category: ");
-            P.getCategory();
-            if(P.getDiscount() != 0){
-                double NewPrice = P.getPrice() - ((P.getDiscount() / 100) * P.getPrice());
-                System.out.print("\nThere is a Discount! Product Price After Discount: ");
-                P.getPrice();
-            }
-            System.out.print("\nProduct Price: ");
-            P.getPrice();
-        }
+    public static Vector<Product> catalog = new Vector<>();
+    public void fillCatalog(){
+        Product P1 = new Product(10.0,"Dark chocolate","chocolate",1.0,0,20);
+        Catalog.catalog.add(P1);
+        Product P2 = new Product(20.0,"White chocolate","chocolate",2.0,1,20);
+        Catalog.catalog.add(P2);
+        Product P3 = new Product(5.0,"Lolipop","Sugar candy",0.0,2,20);
+        Catalog.catalog.add(P3);
+        Product P4 = new Product(30.0,"Caramel Bonbons","bonbon",0.6,3,30);
+        Catalog.catalog.add(P4);
+        Product P5 = new Product(35.0,"chocolate Bonbons","bonbon",0.5,4,20);
+        Catalog.catalog.add(P5);
     }
 }
