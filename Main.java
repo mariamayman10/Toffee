b9af17422463fcda05021ace9d6e5fcffd92b815
Aya Ali <101468593+AyaA1i@users.noreply.github.com>
2023-05-09 23:35:33 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-       Catalog myCatalog = new Catalog();
-       myCatalog.fillCatalog();
-       MySystem ss = new MySystem();
-       ss.EnterApplication();
-       ss.ShowMenu();
-    }
-}
