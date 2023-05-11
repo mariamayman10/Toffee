fa14ca8bf828bb4362ff135672c81f23d12070f2
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2023-05-11 21:29:45 +0300

    Update Main.java

--- Withfiles/Main.java
+++ Withfiles/Main.java
@@ -6,7 +6,7 @@ public class Main {
      * @param args
      * @throws IOException
      */
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException {
         Catalog myCatalog = new Catalog();
         myCatalog.fillCatalog();
         MySystem ss = new MySystem();
