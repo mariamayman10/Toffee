ed842045ae1d93188894ba81e3d2fbbb07a20c4a
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2023-05-12 23:12:00 +0300

    Update Order.java

--- Withfiles/Order.java
+++ Withfiles/Order.java
@@ -3,74 +3,44 @@ import java.util.Map;
 
 public class Order {
     private int Id;
-    private String Status;
-    private String deliveryDate;
-    private String paymentMethod;
-    private double totalPrice;
-    private Customer customer;
+    private String DeliveryDate;
+    private String PaymentMethod;
+    private double TotalPrice;
     private static final Map<Product,Integer> OProducts = new HashMap<>();
     public String getDDate(){
-        return deliveryDate;
+        return DeliveryDate;
+    }
+    public String getPMethod(){
+        return PaymentMethod;
     }
     public double getTotalPrice() {
-        return totalPrice;
+        return TotalPrice;
     }
     public Map<Product, Integer> getOProducts() {
         return OProducts;
     }
-
-    /**
-     *
-     * @param cart
-     */
     public void setOProducts(Map<Product, Integer> cart){
-        OProducts.clear();
         for (Map.Entry<Product, Integer> pair : cart.entrySet()) {
             Product key = pair.getKey();
             Integer value = pair.getValue();
             OProducts.put(key, value);
         }
     }
-
-    /**
-     *
-     * @param deliveryDate
-     */
     public void setDDate(String deliveryDate) {
-        this.deliveryDate = deliveryDate;
+        DeliveryDate = deliveryDate;
     }
-
-    /**
-     *
-     * @param paymentMethod
-     */
     public void setPaymentMethod(String paymentMethod) {
-        this.paymentMethod = paymentMethod;
+        PaymentMethod = paymentMethod;
     }
-
-    /**
-     *
-     * @param status
-     */
-    public void setStatus(String status) {
-        Status = status;
-    }
-
-
-    public void setTotalPrice() {
-        double totalPrice = 0;
+    public void setTotalPrice(double totalPrice) {
+        totalPrice = 0.0;
         for (Map.Entry<Product, Integer> pair : OProducts.entrySet()) {
             Product key = pair.getKey();
             Integer value = pair.getValue();
             totalPrice += value*(key.getPrice() - (key.getDiscount()/100)*key.getPrice());
         }
-        this.totalPrice = totalPrice;
+        TotalPrice = totalPrice;
     }
-
-    /**
-     *
-     * @param id
-     */
     public void setID(int id) {
         Id = id;
     }
